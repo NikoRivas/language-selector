d8bbe164e29e33741c9b02bd11100d70cc88b147
Niko Rivas <rivasniko@gmail.com>
2022-06-19 06:14:34 -0700

    add for loop on scripts page and add first if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
+window.onload = function() {
+  event.preventDefault();
+  let javascriptAnswer = document.getElementById("javascript");
+  javascriptAnswer.style.display = "none";
+  let rubyAnswer = document.getElementById("ruby");
+  rubyAnswer.style.display = "none";
+  let pythonAnswer = document.getElementById("python");
+  pythonAnswer.style.display = "none";
+
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+
+
+
+
+function getInputsByValue(input) {
+const allElements = document.querySelectorAll("input");
+for (i = 0, i<allElements.length, i++) {
+allElements[i]
+if(allElements[i].checked === true) {
+if(allElements[i]) = "javascript" 
+
+
+}
+}
+
+}
+
+
+
+}
+}
+ 
 
 
 
