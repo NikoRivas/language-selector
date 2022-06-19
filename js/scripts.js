5f86c7c48406c50eac2727d3d95cc35fbca4bb45
Niko Rivas <rivasniko@gmail.com>
2022-06-19 06:33:38 -0700

    create all if and else if statements in for loop

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,19 @@ window.onload = function () {
   let form = document.querySelector('form');
   form.onsubmit = function (event) {
     function getInputsByValue(input) {
+      let javascriptCount = 0;
+      let rubyCount = 0;
+      let pythonCount = 0;
       const allElements = document.querySelectorAll('input');
       for (i = 0; i < allElements.length; i++) {
         allElements[i];
         if (allElements[i].checked === true) {
           if (allElements[i] === 'javascript') {
+            javascriptCount++;
+          } else if (allElements[i] === 'ruby') {
+            rubyCount++;
+          } else if (allElements[i] === 'python') {
+            pythonCount++;
           }
         }
       }
